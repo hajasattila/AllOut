90f299bb2cb85fb8c8ba2e914b62b80eb38bfac7
Attila Hajas <64254833+hajasattila@users.noreply.github.com>
2023-04-30 17:21:24 +0200

    Add files via upload

--- AllOut/js/board.js
+++ AllOut/js/board.js
@@ -1,25 +1,25 @@
-// Get the leaderboard table and tbody elements
+
 const leaderboardTable = document.getElementById('rankings');
 const leaderboardTbody = leaderboardTable.querySelector('tbody');
 const empty = document.getElementById('ures');
 
-// Retrieve the game results from localStorage
+
 const gameResults = JSON.parse(localStorage.getItem('gameResults')) || [];
-// Check if gameResults is empty, if so display a message
+
 if (gameResults.length === 0) {
     empty.textContent = 'Nincsenek még adatok rögzítve!';
 }
 
-// Create a function to sort the game results by a given column and update the leaderboard table
+
 const sortByColumn = (columnName) => {
     let sortFunction;
     switch (columnName) {
         case 'Pont':
             sortFunction = (a, b) => {
                 if (a.score === b.score) {
-                    return b.time - a.time;
+                    return a.time - b.time;
                 }
-                return b.score - a.score;
+                return a.score - b.score;
             };
             break;
         case 'Idő':
@@ -33,7 +33,7 @@ const sortByColumn = (columnName) => {
         case 'Lépés':
             sortFunction = (a, b) => {
                 if (a.step === b.step) {
-                    return b.score - a.score;
+                    return a.score - b.score;
                 }
                 return a.step - b.step;
             };
@@ -52,7 +52,7 @@ const sortByColumn = (columnName) => {
     gameResults.sort(sortFunction)
     renderLeaderboard();
 };
-// Add click event listeners to all header cells that call the sortByColumn function with the appropriate column name
+
 const headers = document.querySelectorAll('th');
 headers.forEach((header) => {
     header.addEventListener('click', () => {
@@ -60,7 +60,7 @@ headers.forEach((header) => {
         sortByColumn(columnName);
     });
 });
-// Create a function to render the leaderboard table based on the game results
+
 const renderLeaderboard = (searchText = '') => {
     leaderboardTbody.innerHTML = '';
     gameResults.forEach((result) => {
@@ -89,10 +89,8 @@ const renderLeaderboard = (searchText = '') => {
         }
     });
 };
-// Call the renderLeaderboard function to initially render the leaderboard table
-renderLeaderboard();
 
-// Add an input event listener to the search input element to dynamically update the leaderboard table based on the search text
+renderLeaderboard();
 const searchInput = document.getElementById('search');
 searchInput.addEventListener('input', () => {
     const searchText = searchInput.value.trim();
